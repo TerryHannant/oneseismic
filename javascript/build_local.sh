6013c3160c0bfd880264803f26f8709cf865bd04
Terry Hannant <thannant_github@metafu.net>
2022-04-28 20:51:51 +0200

    WIP

--- javascript/build_local.sh
+++ javascript/build_local.sh
@@ -6,7 +6,9 @@ if [ "$#" -ne 1 ]; then
     echo "build dir missing"
 fi
 
-
+#git clone https://github.com/msgpack/msgpack-c.git
+#cd msgpack-c
+#git checkout cpp_master
 
 BUILD_DIR=$(realpath $1)
 
@@ -17,8 +19,9 @@ emcmake cmake \
     -S core/ -B $BUILD_DIR/core/ \
     -DBUILD_CORE=OFF \
     -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_FLAGS=-I/home/thannant/development/equinor/oneseismic/javascript/deps/boost_1_78_0/ \
     -DCMAKE_CXX_FLAGS=-I/home/thannant/development/equinor/oneseismic/javascript/deps/msgpack-c/include \
-    -DCMAKE_TOOLCHAIN_FILE=/usr/lib/emscripten/cmake/Modules/Platform/Emscripten.cmake \
+    -DCMAKE_TOOLCHAIN_FILE=/home/thannant/.asdf/installs/emsdk/3.1.8/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake \
 
 pushd $BUILD_DIR/core
 emmake make
@@ -29,9 +32,10 @@ emcmake cmake \
     -S javascript/ -B $BUILD_DIR/javascript/ \
     -Doneseismic_DIR=$BUILD_DIR/core/ \
     -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_FLAGS=-I/home/thannant/development/equinor/oneseismic/javascript/deps/boost_1_78_0/ \
     -DCMAKE_CXX_FLAGS=-I/home/thannant/development/equinor/oneseismic/javascript/deps/msgpack-c/include  \
     -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=1 \
-    -DCMAKE_TOOLCHAIN_FILE=/usr/lib/emscripten/cmake/Modules/Platform/Emscripten.cmake \
+    -DCMAKE_TOOLCHAIN_FILE=/home/thannant/.asdf/installs/emsdk/3.1.8/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake \
     "$@" \
 
 pushd $BUILD_DIR/javascript
